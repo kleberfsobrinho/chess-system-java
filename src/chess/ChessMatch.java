3d001cca2bbbcfb73295057c8b0d643ae4800e34
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2020-10-03 14:06:40 -0300

    Queen

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -11,6 +11,7 @@ import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Knight;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -227,6 +228,7 @@ public class ChessMatch {
 		this.placeNewPiece('a', 1, new Rook(board, Color.WHITE));
 		this.placeNewPiece('b', 1, new Knight(board, Color.WHITE));
 		this.placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+		this.placeNewPiece('d', 1, new Queen(board, Color.WHITE));
 		this.placeNewPiece('e', 1, new King(board, Color.WHITE));
 		this.placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
 		this.placeNewPiece('g', 1, new Knight(board, Color.WHITE));
@@ -243,6 +245,7 @@ public class ChessMatch {
 		this.placeNewPiece('a', 8, new Rook(board, Color.BLACK));
 		this.placeNewPiece('b', 8, new Knight(board, Color.BLACK));
 		this.placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+		this.placeNewPiece('d', 8, new Queen(board, Color.BLACK));
 		this.placeNewPiece('e', 8, new King(board, Color.BLACK));
 		this.placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 		this.placeNewPiece('g', 8, new Knight(board, Color.BLACK));

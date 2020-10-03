02fcb331517aadb5d2bd45b8a3ed087718d9b771
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2020-10-03 11:34:43 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -86,7 +86,8 @@ public class ChessMatch {
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = this.board.removePiece(target);
 		this.board.placePiece(p, target);
 		
@@ -94,11 +95,13 @@ public class ChessMatch {
 			this.piecesOnTheBoard.remove(capturedPiece);
 			this.capturesPieces.add(capturedPiece);
 		}
+		
 		return capturedPiece;
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		p.decreaseMoveCount();
 		this.board.placePiece(p, source);
 		
 		if(capturedPiece != null) {
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,18 +7,31 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece{
 	
 	private Color color;
+	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
 	}
-
-	public ChessPosition getChessPosition() {
-		return ChessPosition.fromPosition(position);
-	}
 	
 	public Color getColor() {
 		return color;
+	}
+	
+	public int getMoveCount() {
+		return this.moveCount;
+	}
+	
+	public void increaseMoveCount() {
+		this.moveCount++;
+	}
+	
+	public void decreaseMoveCount() {
+		this.moveCount--;
+	}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
 	}
 	
 	protected boolean isThereOpponentPiece(Position position) {
